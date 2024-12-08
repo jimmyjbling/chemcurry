fb1d38342f8245ece31db9d21f396215c398c883
James Wellnitz <wellnitz.james@gmail.com>
2024-12-08 15:47:14 -0500

    added boron curation function

---
+++ src/mml_chemical_curate/steps/boron.py
@@ -0,0 +1,22 @@
+"""boron curation functions"""
+
+from rdkit.Chem import MolFromSmarts
+
+from ..flags import CurationIssue
+from .base import CurationStep
+
+
+class CurateBoron(CurationStep):
+    """Flags compounds that have Boron in them"""
+
+    def __init__(self):
+        super().__init__()
+        self.issue = CurationIssue.boron
+        self.rank = 4
+
+    def _func(self, molecules):
+        for mol in molecules:
+            if mol.failed_curation:
+                continue
+            if mol.mol.HasSubstructMatch(MolFromSmarts("[#5]")):
+                mol.flag_issue(self.issue)
